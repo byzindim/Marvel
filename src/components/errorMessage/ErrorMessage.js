bd726f5f362a0a3e45be6d2de81ba1effe8badc6
redlinesoft <info@redlinesoft.ru>
2022-12-11 01:56:16 +0700

    content-loader-error-OR

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,8 @@
+import error from '../../resources/img/error.gif'
+
+const ErrorMessage = () => {
+    return (
+        <img src={error} alt="" />
+    )
+}
+export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './randomChar.scss';
 import MarvelService from '../../services/MarvelService';
 import mjolnir from '../../resources/img/mjolnir.png'
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends React.Component {
     constructor(props) {
@@ -9,66 +11,80 @@ class RandomChar extends React.Component {
         this.updateChar();
     }
     state = {
-        name: null,
-        description: null,
-        thumbnail: null,
-        homepage: null,
-        wiki: null,
+       char: {},
+       loading: true,
+       error: false
+
     }
 
     marvelService = new MarvelService();
 
+    onCharLoaded = (char) => {
+        this.setState({char, loading: false})
+    }
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 -1011000) + 1011000);
         this.marvelService
         .getCharacter(id)
-        .then(res => {
-            this.setState({
-                name: res.data.results[0].name,
-                description: res.data.results[0].description,
-                thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-                homepage: res.data.results[0].urls[0].url,
-                wiki: res.data.results[0].urls[1].url,
-            })
+        .then(this.onCharLoaded)
+        .catch(this.onError)
+    }
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
         })
     }
 
     render() {
-        const {name, description, thumbnail, homepage, wiki} = this.state;
-        return (
-            <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">{description}</p>
-                        <div className="randomchar__btns">
-                            <a href= {homepage} className="button button__main">
-                                <div className="inner">HOMEPAGE</div>
-                            </a>
-                            <a href= {wiki} className="button button__secondary">
-                                <div className="inner">WIKI</div>
-                            </a>
-                        </div>
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char}/> : null
+
+            return (
+                <div className="randomchar">
+                    {errorMessage}
+                    {spinner}
+                    {content}
+                    <div className="randomchar__static">
+                        <p className="randomchar__title">
+                            Random character for today!<br/>
+                            Do you want to get to know him better?
+                        </p>
+                        <p className="randomchar__title">
+                            Or choose another one
+                        </p>
+                        <button className="button button__main">
+                            <div className="inner">try it</div>
+                        </button>
+                        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
                     </div>
                 </div>
-                <div className="randomchar__static">
-                    <p className="randomchar__title">
-                        Random character for today!<br/>
-                        Do you want to get to know him better?
-                    </p>
-                    <p className="randomchar__title">
-                        Or choose another one
-                    </p>
-                    <button className="button button__main">
-                        <div className="inner">try it</div>
-                    </button>
-                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-                </div>
-            </div>
-        )
+            )
+        
     }
     
 }
 
+const View = ({char}) => {
+    const  {name, description, thumbnail, homepage, wiki} = char;
+    return (
+        <div className="randomchar__block">
+                        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                        <div className="randomchar__info">
+                            <p className="randomchar__name">{name}</p>
+                            <p className="randomchar__descr">{description}</p>
+                            <div className="randomchar__btns">
+                                <a href= {homepage} className="button button__main">
+                                    <div className="inner">HOMEPAGE</div>
+                                </a>
+                                <a href= {wiki} className="button button__secondary">
+                                    <div className="inner">WIKI</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+    )
+}
 export default RandomChar;
